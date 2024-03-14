57f46e01ced63c71aa9b5afa6952c38e6c7735ab
Johan Back <johan.backs@gmail.com>
2024-03-13 22:29:23 -0300

    add constant-arrival-rate

---
+++ constant-arrival-rate.js
@@ -0,0 +1,19 @@
+import http from "k6/http";
+
+
+export const options = {
+  scenarios: {
+    contacts: {
+      executor: 'constant-arrival-rate',
+      duration: '30s',
+      rate: 30,
+      timeUnit : '1s',
+      preAllocatedVUs: 50,
+    }
+  }
+}
+
+export default function () {
+  http.get('https://test.k6.io/contacts.php');
+  sleep(0.5);
+}
